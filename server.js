bfa69771569436ef5d003f9768a56142a7f2826b
Teresa <tpan@unc.edu>
2022-11-19 18:33:02 -0500

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import {roll} from "../lib/roll.js"; 
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>{
 	res.send("404 NOT FOUND")
@@ -11,12 +11,9 @@ app.get('/app', (req, res)=>{
 	res.send("200 OK")
 });
 
-app.get('/app/roll', (req, res) =>{
-	const sides = 6;
-	const dice = 2;
-	const rolls = 1;
-	var r = roll(sides, dice, rolls);
+app.get('/app/roll', (req, res) ={
+	var r = roll(6, 2, 1);
 	const obj = {sides: sides, dice: dice, rolls: rolls, results: r};
 	res.json(obj);
 });
-app.listen(port, () => console.log('Listening on port 5000...'));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
