b9bdb7baa91b2c310ad5a6be5f27fc2a9fa18446
Teresa <tpan@unc.edu>
2022-11-19 18:47:23 -0500

    app/roll

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const roll = require('../lib/roll.js');
+var roll = require('../lib/roll.js');
 roll = roll();
 const express = require('express');
 const app = express();
@@ -12,9 +12,11 @@ app.get('/app', (req, res)=>{
 	res.send("200 OK")
 });
 
+var r = roll(6,2,1);
+var obj = {sides: 6, dice: 2, rolls:1, results: r};
+const arstring = JSON.stringify(obj);
+
 app.get('/app/roll', (req, res) ={
-	var r = roll(6, 2, 1);
-	const obj = {sides: 6, dice: 2, rolls: 1, results: r};
-	res.json(obj);
+	res.json(arstring);
 });
 app.listen(port, () => console.log(`Listening on port ${port}...`));
