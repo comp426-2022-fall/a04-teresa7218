9196147b477f69a6f7ec317ed0847f3e69c867d5
Teresa <tpan@unc.edu>
2022-11-19 21:25:43 -0500

    app/roll

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ app.get('/app', (req, res)=>{
 	res.send("200 OK")
 });
 
-var r = roll(6,2,1);
-var obj = {sides: 6, dice: 2, rolls:1, results: r};
-const arstring = JSON.stringify(obj);
 
 app.get('/app/roll', (req, res) =>{
-	res.end(arstring);
+	var r = roll(6,2,1);
+	res.json({sides: 6, dice: 2, rolls: 1, results: r});
 });
 app.listen({port}, () => console.log(`Listening on port ${port}...`));
