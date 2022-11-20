95cd0b4ae2bebb1703e59b3e3c8860c9be2cf2ec
Teresa <tpan@unc.edu>
2022-11-19 23:33:26 -0500

    added next

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app = express();
 const {SERVER_PORT: port = 5000} = process.env;
 
 
-app.get('/app/', (req, res)=>{
-	return res.status(200).send("200 OK");
+app.get('/app/', (req, res, next)=>{
+	res.status(200).send("200 OK");
+	next();
 });
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.get('/app/roll/', (req, res) =>{
+app.get('/app/roll/', (req, res, next) =>{
 	var sides = parseInt(req.body.sides)|| 6;
 	var dice = parseInt(req.body.dice)|| 2 ;
 	var rolls = parseInt(req.body.rolls)|| 1;
