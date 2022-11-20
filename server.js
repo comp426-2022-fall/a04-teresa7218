738474ae61a04edbddb8ccadadfe6ec85a76b300
Teresa <tpan@unc.edu>
2022-11-20 00:31:10 -0500

    moved express up

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ import express from 'express';
 import {roll} from './lib/roll.js';
 const app = express();
 const {SERVER_PORT: port = 5000} = process.env;
-
+app.use(express.urlencoded({extended: true}));
 
 app.get('/app/', (req, res, next)=>{
 	res.status(200).send("200 OK");
 	next();
 });
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.get('/app/roll/', (req, res, next) =>{
 	var sides = 6;
 	var dice = 2 ;
