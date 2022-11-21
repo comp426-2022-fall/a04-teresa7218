8dcf999e0330d186a2dcb97ae6200844987241c7
Teresa <tpan@unc.edu>
2022-11-21 10:52:53 -0500

    cant figure out app.post

--- server.js
+++ server.js
@@ -25,18 +25,18 @@ app.get('/app/roll', (req, res, next) => {
 	res.status(200).send(roll(sides,dice,rolls));
 });
 app.post('/app/roll', (req, res, next) =>{
-	let sides = 6;
-	let dice = 2 ;
-	let rolls = 1;
-	if (req.body.sides) {
-        	sides = parseInt(req.body.sides);
-    	}
-    	if (req.body.dice) {
-        	dice = parseInt(req.body.dice);
-    	}
-    	if (req.body.rolls) {
-        	rolls = parseInt(req.body.rolls);
-	}
+	let sides = req.body.sides;
+	let dice = req.body.dice;
+	let rolls = req.body.rolls;
+	//if (req.body.sides) {
+        //	sides = parseInt(req.body.sides);
+    	//}
+    	//if (req.body.dice) {
+        //	dice = parseInt(req.body.dice);
+    	//}
+    	//if (req.body.rolls) {
+        //	rolls = parseInt(req.body.rolls);
+	//}
 	res.setHeader('Content-Type', 'application/json');
 	res.status(200).send(roll(sides,dice,rolls));
 });
@@ -53,6 +53,14 @@ app.get('/app/roll/:sides/:dice', (req, res, next) => {
 
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(roll(sides, dice, 1));
+});
+
+app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+    let sides = req.params.sides||6;
+    let dice = req.params.dice || 2;
+    let rolls = req.params.rolls || 1;
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(sides, dice, rolls));
 });
 
 // Default route
